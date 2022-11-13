6401f696b8497422a5d20361024ca4ab22832860
divyanshuan <vermadivyanshu000@gmail.com>
2022-11-14 00:28:38 +0530

    initial commit

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,28 @@
+import "./expenseitem.css";
+const ExpanseDate = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+};
+
+function ExpenseItem(props) {
+  return (
+    <>
+      <div className="expense-item">
+        <ExpanseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default ExpenseItem;
